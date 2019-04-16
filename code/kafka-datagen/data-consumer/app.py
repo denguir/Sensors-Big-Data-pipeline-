d641f80343109172c44d1cc6e30d908a269b6655
denguir <anass@gmail.com>
2019-04-16 15:34:10 +0200

    kafka to hdfs

--- code/kafka-datagen/data-consumer/app.py
+++ code/kafka-datagen/data-consumer/app.py
@@ -1,14 +1,18 @@
 """Example Kafka consumer."""
 
 import os
+import pandas as pd
+from hdfs import InsecureClient
 from kafka import KafkaConsumer
 
 KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL')
 TOPIC = os.environ.get('TOPIC')
-
+HDFS_URL = os.environ.get('HDFS_URL')
+BATCH_SIZE = os.environ.get('BATCH_SIZE')
 
 if __name__ == '__main__':
-    data_format = "{:{dfmt} {tfmt}} {room_id}-{data_id} {data} {volt}"
+    hdfs_path = '/hadoop-data'
+    client_hdfs = InsecureClient(HDFS_URL)
     consumer = KafkaConsumer(
         TOPIC,
         bootstrap_servers=KAFKA_BROKER_URL,
@@ -16,4 +20,12 @@ if __name__ == '__main__':
     )
 
     for message in consumer:
-        print ("%s:%d:%d: key=%d value=%s" % (message.topic, message.partition, message.offset, int.from_bytes(message.key, 'big'), str(message.value))) 
+        key = int.from_bytes(message.key, 'big')
+        value = message.value.decode('utf-8')
+        # typically, must serialize data before feeding it to HDFS (using dataframe pandas maybe)
+        print("%s:%d:%d: key=%d value=%s" % (message.topic, message.partition, message.offset, key, value))
+        ext = '.txt'
+        filename = str(message.topic) + '_' + str(message.partition) + '_' + str(key) + ext
+        path = os.path.join(hdfs_path, filename)
+        client_hdfs.write(path, data=value, append=True, encoding='utf-8')
+
