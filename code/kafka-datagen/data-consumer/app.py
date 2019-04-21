3ac4483e97a86f5b6a49548d0a0d0b7bb2bc8582
denguir <anass.den@gmail.com>
2019-04-21 08:36:49 +0200

    test spark

--- code/kafka-datagen/data-consumer/app.py
+++ code/kafka-datagen/data-consumer/app.py
@@ -1,31 +1,51 @@
 """Example Kafka consumer."""
 
 import os
+import os.path as osp
 import pandas as pd
-from hdfs import InsecureClient
-from kafka import KafkaConsumer
+from operator import add
+from datetime import datetime
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
 
 KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL')
+ZOOKEEPER_URL = os.environ.get('ZOOKEEPER_URL')
 TOPIC = os.environ.get('TOPIC')
-HDFS_URL = os.environ.get('HDFS_URL')
-BATCH_SIZE = os.environ.get('BATCH_SIZE')
+
+
+def parse_data(line):
+    '''Parses a single line of sensor data'''
+    s = line.strip().split()
+    try:
+        return [{'time': datetime.strptime(osp.join(s[0],s[1]), '%Y-%m-%d/%H:%M:%S'),
+                 'room_id': int(s[2].split('-')[0]),
+                 'data_id': int(s[2].split('-')[1]),
+                 'data': float(s[3]),
+                 'voltage': float(s[4])
+                 }]
+    except Exception as err:
+        print('Wrong line format (%s): %s' % (line, err))
+        return []
+
 
 if __name__ == '__main__':
-    hdfs_path = '/hadoop-data'
-    client_hdfs = InsecureClient(HDFS_URL)
-    consumer = KafkaConsumer(
-        TOPIC,
-        bootstrap_servers=KAFKA_BROKER_URL,
-        api_version=(0, 10, 1)
-    )
 
-    for message in consumer:
-        key = int.from_bytes(message.key, 'big')
-        value = message.value.decode('utf-8')
-        # typically, must serialize data before feeding it to HDFS (using dataframe pandas maybe)
-        print("%s:%d:%d: key=%d value=%s" % (message.topic, message.partition, message.offset, key, value))
-        ext = '.txt'
-        filename = str(message.topic) + '_' + str(message.partition) + '_' + str(key) + ext
-        path = os.path.join(hdfs_path, filename)
-        client_hdfs.write(path, data=value, append=True, encoding='utf-8')
+    sc = SparkContext("local[*]", "SpeedLayer")
+    sc.setLogLevel("WARN")
+    ssc = StreamingContext(sc, 5)
+    inputStream = KafkaUtils.createStream(ssc, ZOOKEEPER_URL, 
+                                        'streaming-consumer', {TOPIC: 1})
+    
+    # data comes from Kafka as a key, value pairs
+    data = inputStream.map(lambda x: x[1]).flatMap(parse_data).collect()
+    data.pprint()
+'''
+    temp_freq = data.filter(lambda d: d['data_id'] == 0)\
+                    .groupBy(lambda d: round(d['temp'], 1))\
+                    .map(lambda v: 1).window(3600, 60)\
+                    .reduce(add)
+    temp_freq.pprint()
+'''
+
 
