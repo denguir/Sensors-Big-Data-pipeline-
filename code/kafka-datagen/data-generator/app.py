7d890afa4be0074162d54ce7c462b92933b1ddf6
Ubuntu <ubuntu@ip-172-31-94-130.ec2.internal>
2019-05-03 20:05:28 +0000

    data generator update

--- code/kafka-datagen/data-generator/app.py
+++ code/kafka-datagen/data-generator/app.py
@@ -9,17 +9,19 @@ KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL')
 DATA_PER_SECOND = os.environ.get('DATA_PER_SECOND')
 TOPIC = os.environ.get('TOPIC')
 SLEEP_TIME = 1. / float(DATA_PER_SECOND)
+NUM_SENSORS = 54
+NUM_TYPE_SENSORS = 4
 
-def generate_data(data_id):
+def generate_data(data_id, max_id):
     """Generate a random distribution depending on data_id:
     data_id = 0 --> temperature
     data_id = 1 --> humidity
     data_id = 2 --> light
     """
-    assert data_id in range(4)
+    assert data_id in range(max_id)
     if data_id == 0:
         # temperature
-        data = np.random.normal(20., 5.)
+        data = np.random.normal(20., 2.)
     elif data_id == 1:
         # humidity
         data = np.random.uniform(0., 100.)
@@ -57,16 +59,20 @@ if __name__ == '__main__':
     data_format = "{:{dfmt} {tfmt}} {mun_id}-{room_id}-{data_id} {data} {volt}"
     while True:
         now = datetime.now()
-        room_id = np.random.randint(1, 54)
-        mun_id = room_id % 19 # there is 19 municipalities in Brussels
-        voltage = np.around(np.random.uniform(0., 5.), decimals=4)
-        for i in range(4):
-            sensor_data = generate_data(i)
-            data = data_format.format(now, dfmt='%Y-%m-%d', tfmt='%H:%M:%S', mun_id=mun_id, 
-                                room_id=room_id, data_id=i, data=sensor_data, volt=voltage)
-            print(data)
+        # room_id = np.random.randint(1, NUM_SENSORS)
+        # mun_id = room_id % 19 # there is 19 municipalities in Brussels
+        for room_id in range(NUM_SENSORS):
+            mun_id = room_id % 19 # there is 19 municipalities in Brussels
+            for i in range(NUM_TYPE_SENSORS):
+                if np.random.randint(0, 100) > 30:
+                    voltage = np.around(np.random.uniform(0., 5.), decimals=4)
+                    sensor_data = generate_data(i, NUM_TYPE_SENSORS)
+                    data = data_format.format(now, dfmt='%Y-%m-%d', tfmt='%H:%M:%S', mun_id=mun_id, 
+                                        room_id=room_id, data_id=i, data=sensor_data, volt=voltage)
+                    print(data)
             producer.send(TOPIC, key=bytes([room_id]), value=bytes(data, encoding='utf-8'))
         producer.flush()
         time.sleep(SLEEP_TIME)
+        
 
     
